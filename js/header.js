03637738cf0c256bf65d2da8b7917db26f69b349
Pavel Kuprianov <97345976+PavelKuprianov@users.noreply.github.com>
2022-11-16 14:26:31 +0300

    again

--- js/header.js
+++ js/header.js
@@ -1,32 +1,32 @@
 
-document.addEventListener('DOMContentLoaded', () => { // DOM готов к взаимодейтсвию
+// document.addEventListener('DOMContentLoaded', () => { // DOM готов к взаимодейтсвию
 
-  const onScrollHeader = () => { // объявляем основную функцию onScrollHeader
+//   const onScrollHeader = () => { // объявляем основную функцию onScrollHeader
 
-    const header = document.querySelector('.header__menu') // находим header и записываем в константу
+//     const header = document.querySelector('.header__menu') // находим header и записываем в константу
 
-    let prevScroll = window.pageYOffset // узнаем на сколько была прокручена страница ранее
-    let currentScroll // на сколько прокручена страница сейчас (пока нет значения)
+//     let prevScroll = window.pageYOffset // узнаем на сколько была прокручена страница ранее
+//     let currentScroll // на сколько прокручена страница сейчас (пока нет значения)
 
-    window.addEventListener('scroll', () => { // при прокрутке страницы
+//     window.addEventListener('scroll', () => { // при прокрутке страницы
 
-      currentScroll = window.pageYOffset // узнаем на сколько прокрутили страницу
+//       currentScroll = window.pageYOffset // узнаем на сколько прокрутили страницу
 
-      const headerHidden = () => header.classList.contains('header_hidden') // узнаем скрыт header или нет
+//       const headerHidden = () => header.classList.contains('header_hidden') // узнаем скрыт header или нет
 
-      if (currentScroll > prevScroll && !headerHidden()) { // если прокручиваем страницу вниз и header не скрыт
-        header.classList.add('header_hidden') // то скрываем header
-      }
-      if (currentScroll < prevScroll && headerHidden()) { // если прокручиваем страницу вверх и header скрыт
-        header.classList.remove('header_hidden') // то отображаем header
-      }
+//       if (currentScroll > prevScroll && !headerHidden()) { // если прокручиваем страницу вниз и header не скрыт
+//         header.classList.add('header_hidden') // то скрываем header
+//       }
+//       if (currentScroll < prevScroll && headerHidden()) { // если прокручиваем страницу вверх и header скрыт
+//         header.classList.remove('header_hidden') // то отображаем header
+//       }
 
-      prevScroll = currentScroll // записываем на сколько прокручена страница на данный момент
+//       prevScroll = currentScroll // записываем на сколько прокручена страница на данный момент
 
-    })
-  }
-  onScrollHeader() // вызываем основную функцию onScrollHeader
-});
+//     })
+//   }
+//   onScrollHeader() // вызываем основную функцию onScrollHeader
+// });
 
 //Ленивая загрузка изображений
 
